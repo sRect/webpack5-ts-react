ce1c403fc9819cdca3c28f62771facb2aec1ba30
srect <fcqun@sina.cn>
2021-07-14 23:59:57 +0800

    feat: 添加react类型检查

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
-function calc(a:number, b:number):number {
-  return a + b;
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const num = calc(1, 2);
-console.log(num)
+ReactDOM.render(<div>react+ts</div>, document.getElementById('app'))
+
+
+// function calc(a:number, b:number):number {
+//   return a + b;
+// }
+
+// const num = calc(1, 2);
+// console.log(num);
